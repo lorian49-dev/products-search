bc2e4dd590b0ed36b95d91be4e4f4352fb981f68
David Alejandro Gutierrez <120154242+lorian49-dev@users.noreply.github.com>
2025-10-29 20:57:16 -0500

    agregado del dashboard especial de vendedor
    Se le agrego la pagina de dashboard de vendedor, en el cual se le daran dos opciones de inicio

--- seller/dashboardSeller.php
+++ seller/dashboardSeller.php
@@ -4,9 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vendedor</title>
-    <link rel="shortcut icon" href="SOURCES/ICONOS-LOGOS/ico.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../SOURCES/ICONOS-LOGOS/ico.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../SOURCES/ICONOS-LOGOS/fontawesome-free-7.1.0-web/css/all.css">
+    <link rel="stylesheet" href="dashboard.css">
 </head>
 <body>
-    
+    <header>
+        <span id="logo-hermes-home"><h1>HERMES</h1></span>
+        <span><i class="far fa-home"></i></span>
+    </header>
+    <div class="container">
+        <div class="content">
+            <div class="login">
+             <h2>Ya eres Vendedor?</h2>
+             <button class="manageBussines">
+                Gestionar Negocio
+             </button>
+            </div>
+            <div class="register">
+             <h2>Aun no eres vendedor?</h2>
+             <button class="newBussines">
+                Comenzar a vender
+             </button>
+            </div>
+        </div>
+    </div>
+    <main>
+        <h2>Como comenzar a vender?</h2>
+        <section>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint rem quasi, voluptatibus omnis porro fugit! Nesciunt nobis nulla eum quaerat quia dignissimos officiis corporis quas optio cum, odio temporibus debitis amet quasi consequatur, tempore cupiditate excepturi voluptatum! Doloribus nihil nemo magni voluptates impedit soluta. Temporibus velit dicta itaque blanditiis dolore!
+            </p>
+             <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint rem quasi, voluptatibus omnis porro fugit! Nesciunt nobis nulla eum quaerat quia dignissimos officiis corporis quas optio cum, odio temporibus debitis amet quasi consequatur, tempore cupiditate excepturi voluptatum! Doloribus nihil nemo magni voluptates impedit soluta. Temporibus velit dicta itaque blanditiis dolore!
+            </p>
+             <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint rem quasi, voluptatibus omnis porro fugit! Nesciunt nobis nulla eum quaerat quia dignissimos officiis corporis quas optio cum, odio temporibus debitis amet quasi consequatur, tempore cupiditate excepturi voluptatum! Doloribus nihil nemo magni voluptates impedit soluta. Temporibus velit dicta itaque blanditiis dolore!
+            </p>
+             <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint rem quasi, voluptatibus omnis porro fugit! Nesciunt nobis nulla eum quaerat quia dignissimos officiis corporis quas optio cum, odio temporibus debitis amet quasi consequatur, tempore cupiditate excepturi voluptatum! Doloribus nihil nemo magni voluptates impedit soluta. Temporibus velit dicta itaque blanditiis dolore!
+            </p>
+        </section>
+        <aside>
+            <h2>Sumario</h2>
+            <ol>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ol>
+        </aside>
+    </main>
 </body>
 </html>
