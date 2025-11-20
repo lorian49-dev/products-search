20a3d501e562ba281aea0cbbadd1d321bf5c9660
David Alejandro Gutierrez <120154242+lorian49-dev@users.noreply.github.com>
2025-11-19 19:33:28 -0500

    seller Update

--- seller/dashboardSeller.php
+++ seller/dashboardSeller.php
@@ -7,6 +7,7 @@
     <link rel="shortcut icon" href="../SOURCES/ICONOS-LOGOS/ico.ico" type="image/x-icon">
     <link rel="stylesheet" href="../SOURCES/ICONOS-LOGOS/fontawesome-free-7.1.0-web/css/all.css">
     <link rel="stylesheet" href="dashboard.css">
+    <script src="https://unpkg.com/scrollreveal"></script>
 </head>
 <body>
 
@@ -39,24 +40,40 @@
     </div>
 
     <main>
-        <h2>¿Cómo comenzar a vender?</h2>
-        <section></section>
-
-        <aside>
-            <h2>Sumario</h2>
-            <ol>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ol>
-        </aside>
+        
+     <h2 class="scrolling">Hermes Click&Sell</h2>
+     <div class="scrolling">
+     <section class="scrolling-container-section">
+      <p>
+    <strong>Hermes Click&Sell</strong> nació con la misión de conectar a personas que desean 
+    comercializar sus productos, ya sean nuevos o usados, con un público que busca siempre 
+    la mejor calidad y confianza en cada compra. <br>
+  </p>
+  <p>
+    Nuestra plataforma <strong>Hermes Click&Go</strong> ofrece un espacio dinámico y seguro 
+    donde vendedores independientes y empresas pueden alojar sus tiendas virtuales, 
+    facilitando la interacción directa con potenciales clientes.
+  </p>
+  <p>
+    Con <strong>Hermes Click&Sell</strong> unificamos la experiencia de compra y venta, 
+    brindando herramientas que permiten:
+  </p>
+  <ul>
+    <li>Publicar productos de manera rápida y sencilla.</li>
+    <li>Acceder a una comunidad activa de compradores interesados.</li>
+    <li>Garantizar transacciones seguras y transparentes.</li>
+    <li>Impulsar la visibilidad de tu negocio en un entorno digital confiable.</li>
+  </ul>
+  <p>
+    Ya seas un emprendedor que inicia su camino o una empresa consolidada, 
+    <strong>Hermes Click&Sell</strong> es tu aliado para crecer en el mundo del comercio electrónico.
+  </p>
+     </section>
+     <aside class="scrolling-container-aside">
+        <img src="../SOURCES/IMAGENES/ILUSTRACIONES/girl-pc.svg" alt="imagen de señorita frente a un portatil" width="300px">
+     </aside>
+     </div>
+     
     </main>
 
     <script src="dashboardseller2.js"></script>
--- seller/dashboardseller2.js
+++ seller/dashboardseller2.js
@@ -3,3 +3,9 @@ let logoPage = document.getElementById('logo-hermes-home')
 logoPage.addEventListener('click', ()=>{
     window.location.href = "../home.php"
 })
+
+ScrollReveal().reveal('.scrolling', {
+    delay: 50,
+    duration: 1500,
+    reset: false
+});
